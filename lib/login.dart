dfadaf0fce6dcf63802619f21e085a327cd9955d
Darrylwin <logossoudarryl20@gmail.com>
2024-05-17 12:33:48 +0000

    Création des champs Email et pPassword ainsi que le background

--- lib/login.dart
+++ lib/login.dart
@@ -13,8 +13,51 @@ class _MyLoginState extends State<MyLogin> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/login.png')
+          image: AssetImage('assets/login.png'), fit: BoxFit.cover
         )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 100, top: 130),
+              child: Text("Welcome\nBack",style: TextStyle(
+                color: Colors.white,
+                fontSize: 40
+              ),),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.5,
+              right: 35,
+              left: 35),
+              child: Column(
+                children: [
+                  Textfield(
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink.shade100,
+                      filled: true,
+                      hintText: "Email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                  ),
+                  Textfield(
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink.shade100,
+                      filled: true,
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
